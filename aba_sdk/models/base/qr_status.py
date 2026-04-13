e0ba6da646c36653dd18c34a2a80d519480a24a3
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-04-13 18:58:57 +0700

    refactor: restructure QRStatus and CheckTransactionStatus to inherit from BaseStatus

--- aba_sdk/models/base/qr_status.py
+++ aba_sdk/models/base/qr_status.py
@@ -1,21 +1,50 @@
 from dataclasses import dataclass
 
 @dataclass
-class QRStatus:
+class BaseStatus:
     code: str
     message: str
-    trace_id: str
 
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            code=data.get("code", ""),
+            message=data.get("message", "")
+        )
+
+# QR status response
+@dataclass
+class QRStatus(BaseStatus):
+    trace_id: str
+    
     @classmethod
     def from_dict(cls, data: dict) -> "QRStatus":
         return cls(
             code=data.get("code", ""),
             message=data.get("message", ""),
             trace_id=data.get("trace_id", "")
-        )       
+        )
+    @property
+    def is_success(self) -> bool:
+        
+        # ABA use "0" for success
+        return self.code == "0"
+
+
+# check transaction status response
+@dataclass
+class CheckTransactionStatus(BaseStatus):
+    tran_id: str
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "CheckTransactionStatus":
+        return cls(
+            code=data.get("code", ""),
+            message=data.get("message", ""),
+            tran_id=data.get("tran_id", "")
+        )
     @property
     def is_success(self) -> bool:
 
-        # ABA use "0" for success
-        return self.code == "0"
-  
+        # ABA use "00" for success
+        return self.code == "00"
