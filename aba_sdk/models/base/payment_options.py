14ba85c7b1593d6d85f11f20fb28416da4631908
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-04-13 19:44:51 +0700

    feat: update PaymentOption docstring to clarify supported payment methods

--- aba_sdk/models/base/payment_options.py
+++ aba_sdk/models/base/payment_options.py
@@ -2,6 +2,11 @@ from enum import Enum
 
 class PaymentOption(str, Enum):
 
+    ''' Supported payment options:
+        - abapay_khqr : Payway will response ABA KHQR.
+        - wechat : PayWay will respond with a WeChat QR (only for USD transactions)
+        - alipay : PayWay will respond with an Alipay QR (only for USD transactions) '''
+
     ABAPAY = "abapay"
     KHQR = "khqr"
     ABAPAY_KHQR = "abapay_khqr"
