6575ac6befdcaeed624a554ca41c1f6886f2092b
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-04-16 13:28:49 +0700

    feat: add QRImageTemplate enum for QR image templates

---
+++ aba_sdk/models/base/qrImage_template.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class QRImageTemplate(str, Enum):
+    TEMPLATE1 = "template1"
+    TEMPLATE1_COLOR = "template1_color"
+    TEMPLATE2 = "template2"
+    TEMPLATE2_COLOR = "template2_color"
+    TEMPLATE4 = "template4"
+    TEMPLATE3_COLOR = "template3_color"
