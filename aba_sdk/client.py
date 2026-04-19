178518e72b429e92ad1f54d60acb600c893884c7
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-04-19 22:51:49 +0700

    fix: methods reuseable code transaction

--- aba_sdk/client.py
+++ aba_sdk/client.py
@@ -1,12 +1,14 @@
 from .config import PaywayConfig, Environment
 from .api.qr import QRClient
-from .api.transaction_client import CheckTransactionClient
+from .api.transaction_client import TransactionClient
 
 class PaywayClient:
     def __init__(self, config: PaywayConfig) -> None:
         self.config = config
         self.qr = QRClient(config)
-        self.check_transaction=CheckTransactionClient(config)
+        transaction_client = TransactionClient(config)
+        self.check_transaction = transaction_client
+        self.close_transaction = transaction_client
 
     def __repr__(self) -> str:
         env = self.config.env.value
